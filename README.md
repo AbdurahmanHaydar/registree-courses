c1b3d390c49aa0d1135a2f97f0b7fa0e5f1dbb4c
Abdurahman Haydar <your_email_address@example.com>
2020-05-17 15:25:21 +0200

    readme update

--- README.md
+++ README.md
@@ -16,6 +16,18 @@ npm run start
 ```
 
 
+## User Experience choices
+
+1 - Initially when the page is loaded, the user/ recruiter is shown all the degrees in alphabetical order.
+
+
+2 -  The recruiter then has the option to filter by either degree name, degree level or degree duration.
+
+
+3 -  If the recruiter wants all final year engineering students, he/she will Select "Engineering" under the Names filter then select duration period whether its 3 years or 4 years under the "Duration" filter.
+
+4 - if the recruiter wants only master students , The "Level" filter will have the option of only showing the desired leve and the recruiter may select the "Master"  option to see only Master students.
+
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var request = require('request');
 var path = require('path');
 const app = express()
 const port = 3001
- 
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/api', function(req, res){
@@ -11,11 +11,11 @@ app.get('/api', function(req, res){
     var arr = []
 
 
-    request('https://registree-coding-challenge.glitch.me/stanford', function(error, response, body) {
+    request('https://registree-coding-challenge.glitch.me/cornell' , function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var data1 = JSON.parse(body);
 
-            request('https://registree-coding-challenge.glitch.me/cornell', function(error, response, body) {
+            request('https://registree-coding-challenge.glitch.me/stanford' , function(error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var data2 = JSON.parse(body);
                     console.log('data1', data1, 'data2', data2);
